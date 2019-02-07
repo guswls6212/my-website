64fd8b8060ea68f92aa7a7f5a8a42bc02d318ca2
guswls6212 <guswls6212@gmail.com>
2019-02-07 23:24:44 +0900

    Django Tutorial Video4 until 16:38

--- leeblog/views.py
+++ leeblog/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Lee',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Han',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 # Create your views here.
 def index(request):
     return render(request, 'leeblog/index.html')
+
+def home(request):
+    context ={
+        'posts': posts
+    }
+    return render(request, 'leeblog/home.html', context)
+
+def about(request):
+    return render(request, 'leeblog/about.html', {'title': 'About'})
